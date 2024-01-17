c89b77f170a8b9b109e4808e5d2253000682f496
Yifan Jiang <jiangyifan_sh@outlook.com>
2024-01-17 22:55:41 +0000

    update navbar underline

--- assets/js/navbar_underline.js
+++ assets/js/navbar_underline.js
@@ -1,14 +1,41 @@
 const navbar = document.querySelector(".navbar-nav");
-const first_nav = navbar.querySelector(".nav-link");
+const navLinks = navbar.querySelectorAll(".nav-link");
+const activeNavItem = navbar.querySelector(".nav-item.active > .nav-link");
 
-if (navbar) {
-  navbar.addEventListener("mouseover", (event) => {
-    start_pos = first_nav.offsetLeft;
-    if (event.target.classList.contains("nav-link")) {
-      navbar.style.setProperty("--underline-width", `${0.9 * event.target.offsetWidth}px`);
-      navbar.style.setProperty("--underline-offset-x", `${event.target.offsetLeft - start_pos + 0.05 * event.target.offsetWidth}px`);
-    }
-  });
+let start_pos = navLinks[0].offsetLeft;
+let active_nav_width = activeNavItem.offsetWidth;
+let active_nav_offset = activeNavItem.offsetLeft;
 
-  navbar.addEventListener("mouseleave", () => navbar.style.setProperty("--underline-width", "0"));
+function setUnderlineStyles(width, offset) {
+  navbar.style.setProperty("--underline-width", `${0.9 * width}px`);
+  navbar.style.setProperty("--underline-offset-x", `${offset - start_pos + 0.05 * width}px`);
 }
+
+function updateUnderlineStyles() {
+  setUnderlineStyles(active_nav_width, active_nav_offset);
+}
+
+function handleNavbarMouseover(event) {
+  if (event.target.classList.contains("nav-link")) {
+    setUnderlineStyles(event.target.offsetWidth, event.target.offsetLeft);
+  }
+}
+
+function handleNavbarMouseleave() {
+  updateUnderlineStyles();
+}
+
+function handleWindowResize() {
+  start_pos = navLinks[0].offsetLeft;
+  active_nav_width = activeNavItem.offsetWidth;
+  active_nav_offset = activeNavItem.offsetLeft;
+  updateUnderlineStyles();
+}
+
+// Initial setup
+updateUnderlineStyles();
+
+// Event listeners
+navbar.addEventListener("mouseover", handleNavbarMouseover);
+navbar.addEventListener("mouseleave", handleNavbarMouseleave);
+window.addEventListener("resize", handleWindowResize);
