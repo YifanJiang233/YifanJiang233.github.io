5491a3b1012d32ae1df0bb72151e72bf29b9abff
Yifan Jiang <jiangyifan_sh@outlook.com>
2024-01-18 15:29:11 +0000

    uppdate logic for navbar underline

--- assets/js/navbar_underline.js
+++ assets/js/navbar_underline.js
@@ -1,26 +1,30 @@
 const navbar = document.querySelector(".navbar-nav");
 const navLinks = navbar.querySelectorAll(".nav-link");
 const activeNavItem = navbar.querySelector(".nav-item.active > .nav-link");
+const navbarToggler = document.querySelector(".navbar-toggler");
+
+let flag = true;
 
 let start_pos = navLinks[0].offsetLeft;
 let active_nav_width = activeNavItem.offsetWidth;
 let active_nav_offset = activeNavItem.offsetLeft;
 
 function setUnderlineStyles(width, offset) {
-  navbar.style.setProperty("--underline-width", `${0.9 * width}px`);
-  navbar.style.setProperty("--underline-offset-x", `${offset - start_pos + 0.05 * width}px`);
-}
-
-function updateUnderlineStyles() {
-  let navcollapse = document.querySelector(".navbar-collapse");
-  if (!navcollapse.classList.contains("show") & !navcollapse.classList.contains("collapsing")) {
-    setUnderlineStyles(active_nav_width, active_nav_offset);
+  if (flag) {
+    navbar.style.setProperty("--underline-width", `${0.9 * width}px`);
+    navbar.style.setProperty("--underline-offset-x", `${offset - start_pos + 0.05 * width}px`);
+  } else {
+    navbar.style.setProperty("--underline-width", `0px`);
+    navbar.style.setProperty("--underline-offset-x", `0px`);
   }
 }
 
+function updateUnderlineStyles() {
+  setUnderlineStyles(active_nav_width, active_nav_offset);
+}
+
 function handleNavbarMouseover(event) {
-  let navcollapse = document.querySelector(".navbar-collapse");
-  if (event.target.classList.contains("nav-link") & !navcollapse.classList.contains("show") & !navcollapse.classList.contains("collapsing")) {
+  if (flag && event.target.classList.contains("nav-link")) {
     setUnderlineStyles(event.target.offsetWidth, event.target.offsetLeft);
   }
 }
@@ -33,8 +37,21 @@ function handleWindowResize() {
   start_pos = navLinks[0].offsetLeft;
   active_nav_width = activeNavItem.offsetWidth;
   active_nav_offset = activeNavItem.offsetLeft;
+
+  if (window.innerWidth < 576) {
+    flag = false;
+  } else {
+    flag = true;
+  }
+
   updateUnderlineStyles();
 }
+
+function handleClickToggler() {
+  flag = false;
+}
+
+navbarToggler.addEventListener("click", handleClickToggler);
 
 // Initial setup
 updateUnderlineStyles();
@@ -42,4 +59,4 @@ updateUnderlineStyles();
 // Event listeners
 navbar.addEventListener("mouseover", handleNavbarMouseover);
 navbar.addEventListener("mouseleave", handleNavbarMouseleave);
-window.addEventListener("resize", handleWindowResize);
+window.addEventListener("resize", handleWindowResize, 100);
